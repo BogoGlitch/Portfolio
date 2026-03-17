ec4cb46fed53b568535dce5775ab165a63b35fda
Sean Bogolin <BogoGlitch@gmail.com>
2026-03-17 12:49:50 -0500

    feat: add Next.js frontend and implement projects read page

---
+++ portfolio-web/src/app/projects/page.tsx
@@ -0,0 +1,34 @@
+import { getProjects } from "@/lib/api";
+
+type Project = {
+  id: number;
+  name: string;
+  shortDescription: string;
+  technologies: {
+    id: number;
+    name: string;
+    slug: string;
+  }[];
+};
+
+export default async function ProjectsPage() {
+  const projects: Project[] = await getProjects();
+  return (
+    <main>
+      <h1>Projects</h1>
+
+      {projects.map((project) => (
+        <section key={project.id}>
+          <h2>{project.name}</h2>
+          <p>{project.shortDescription}</p>
+          <p>
+            Technologies:{" "}
+            {project.technologies.length > 0
+              ? project.technologies.map((t) => t.name).join(", ")
+              : "None currently associated with this project."}{" "}
+          </p>
+        </section>
+      ))}
+    </main>
+  );
+}
